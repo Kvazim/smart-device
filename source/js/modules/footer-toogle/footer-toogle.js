04d46e4edc10ef9f54bc666b6fa89dad5cf5a819
Vladimir <mesvladimir@yandex.ru>
2022-10-23 04:01:32 +0300

    начал разметку формы

--- source/js/modules/footer-toogle/footer-toogle.js
+++ source/js/modules/footer-toogle/footer-toogle.js
@@ -1,19 +1,37 @@
 import {titleWrapper} from '../nojs/nojs';
 
 const onOpenAccardion = (element) => {
-  element.classList.add('is-active');
+  if (element.classList.contains('toogle') || element.classList.contains('title-wrapper__title')) {
+    element.parentNode.classList.add('is-active');
+  } else {
+    element.classList.add('is-active');
+  }
 };
 
 const onCloseAccardion = (element) => {
-  element.classList.remove('is-active');
+  if (element.parentNode.classList.contains('is-active')) {
+    element.parentNode.classList.remove('is-active');
+  } else {
+    element.classList.remove('is-active');
+  }
 };
 
 const onButtonClick = (e) => {
   const element = e.target;
+  const availability = document.querySelectorAll('.title-wrapper');
 
-  if (element.classList.contains('is-active')) {
+  if (availability.length === 0) {
+    onOpenAccardion(element);
+  }
+
+  if (element.classList.contains('is-active') || element.parentNode.classList.contains('is-active')) {
     onCloseAccardion(element);
-  } else {
+  }
+
+  if (!element.parentNode.classList.contains('is-active') || !element.classList.contains('is-active')) {
+    availability.forEach((item)=> {
+      item.classList.remove('is-active');
+    });
     onOpenAccardion(element);
   }
 };
