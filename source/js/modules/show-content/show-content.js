f9e989fc3a7453d967bb88874930fee8c233e883
Vladimir <mesvladimir@yandex.ru>
2022-10-28 19:06:13 +0300

    fix bugs

--- source/js/modules/show-content/show-content.js
+++ source/js/modules/show-content/show-content.js
@@ -1,17 +1,13 @@
 const aboutbtn = document.querySelector('.about-content__button');
 const aboutContent = document.querySelector('.about-content__subtitle');
 
-const aboutHeight = aboutContent.style.height;
-
 const showContent = () => {
   aboutContent.classList.add('is-show');
-  aboutContent.style.height = 'auto';
   aboutbtn.textContent = 'Свернуть';
 };
 
 const closeContent = () => {
   aboutContent.classList.remove('is-show');
-  aboutContent.style.height = aboutHeight;
   aboutbtn.textContent = 'подробнее';
 };
 
